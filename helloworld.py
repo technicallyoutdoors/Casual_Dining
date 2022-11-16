0f3e9ac41dec376ce0c80ba3b82bcf4054151192
Matt Miller <matt.miller@technicallyoutdoors.com>
2022-11-15 22:15:17 -0600

    Added a conditional elif statement

--- helloworld.py
+++ helloworld.py
@@ -1,5 +1,7 @@
-x = input("what is your favorite drink?")
+x = input("what is your favorite drink? ")
+print("Your favorite drink is, " + x)
 
-print("my favorite drink is, " + x)
-
-
+if x == ("Whiskey").casefold():
+    print('Your taste is super great, here take a free cigar =`')
+else:
+    print('You have bad taste')
