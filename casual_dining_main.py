46f298469de7f1f6d6054f7c56220d22655d949b
Matthew Miller <mattmill1217@outlook.com>
2022-11-24 17:22:20 -0600

    added the find a restaurant function to the causual dining project

--- casual_dining_main.py
+++ casual_dining_main.py
@@ -1,11 +1,20 @@
 def dining_types():
     print("this is going to find all dining types that are casual")
 
+def find_restauraunt():
+    type = input("What kind of resturaunt do you want to find? " )
+    output = str("Here is a list of the ", type, "restaruants within a 10 mile radius of your area ")
+find_restauraunt()
+
 
 dining_types()
 
 #added comments
 def hello_world():
-    var1 = str('hello, how are you')
-    print(hello_world())
+    hello = str('hello, how are you')
+    
+hello_world()
+
+print(hello_world())
+
 
