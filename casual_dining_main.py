caa09b63c38e4e10050788800a2f65ab5ea4aae2
Matt Miller <matt.miller@technicallyoutdoors.com>
2022-11-26 00:08:44 -0600

    fixed api bug and added the dependencies to the webscrape function

--- casual_dining_main.py
+++ casual_dining_main.py
@@ -9,9 +9,10 @@ dining_types()
 
 #web scraping function for dinning types.
 #-cjosh
-def webscrape_dinning_types(): 
+def webscrape_dinning_types():
+    from bs4 import BeautifulSoup 
     import requests
-    url = "https://www.google.com/search?rlz=1C1CHBF_enUS916US916&sxsrf=ALiCzsblr3vdeNM7Crb2YxKcokERB8vrIQ:1669440028825&q=North+Italia&ludocid=1882766253883301680&gsas=1&lsig=AB86z5UbFiwXMXCE3pI1C6IeWY-5&sa=X&ved=2ahUKEwj5_pDfjMv7AhXvm2oFHXTcCq8QhecIegQIahAN&biw=2560&bih=1307&dpr=1"
+    url = "http://ajax.googleapis.com/ajax/services/search?rlz=1C1CHBF_enUS916US916&sxsrf=ALiCzsblr3vdeNM7Crb2YxKcokERB8vrIQ:1669440028825&q=North+Italia&ludocid=1882766253883301680&gsas=1&lsig=AB86z5UbFiwXMXCE3pI1C6IeWY-5&sa=X&ved=2ahUKEwj5_pDfjMv7AhXvm2oFHXTcCq8QhecIegQIahAN&biw=2560&bih=1307&dpr=1"
     result = requests.get(url).text 
     doc = BeautifulSoup(result, "html.parser") 
     span = doc.span
