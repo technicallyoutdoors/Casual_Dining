a4aad74d3310e79af8c7fcb9d8d70ba28d04a8f7
Matt Miller <matt.miller@technicallyoutdoors.com>
2022-11-25 22:34:42 -0600

    added comments and created functions for finding dining types, fixed one bug

--- casual_dining_main.py
+++ casual_dining_main.py
@@ -1,20 +1,14 @@
+#function to explain the program
 def dining_types():
-    print("this is going to find all dining types that are casual")
+    print("This program will find all the type of restaurant's from your input")
+dining_types()
+
+#function to find the dining type from user input
 
 def find_restaurant():
     type = input("What kind of restaurant do you want to find? " )
-    output = str("Here is a list of the ", type, "restaurants within a 10 mile radius of your area ")
+    output = str("Here is a list of the " + type + " restaurants within a 10 mile radius of your area")
+    print(output)
 find_restaurant()
-
-
-dining_types()
-
-#added comments
-def hello_world():
-    hello = str('hello, how are you')
-    
-hello_world()
-
-print(hello_world())
 
 
