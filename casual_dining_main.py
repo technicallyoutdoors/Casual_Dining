baea55ea34e0eb69fbe74f8b4170b8ad547b6a6d
Josh Remington <josh.d.remington@gmail.com>
2022-11-25 23:55:47 -0600

    added web scrape function andyn html

--- casual_dining_main.py
+++ casual_dining_main.py
@@ -1,10 +1,25 @@
+from bs4 import BeautifulSoup
+import requests
+
 #function to explain the program
 def dining_types():
     print("This program will find all the type of restaurant's from your input")
 dining_types()
 
-#function to find the dining type from user input
 
+#web scraping function for dinning types.
+#-cjosh
+def webscrape_dinning_types(): 
+    import requests
+    url = "https://www.google.com/search?rlz=1C1CHBF_enUS916US916&sxsrf=ALiCzsblr3vdeNM7Crb2YxKcokERB8vrIQ:1669440028825&q=North+Italia&ludocid=1882766253883301680&gsas=1&lsig=AB86z5UbFiwXMXCE3pI1C6IeWY-5&sa=X&ved=2ahUKEwj5_pDfjMv7AhXvm2oFHXTcCq8QhecIegQIahAN&biw=2560&bih=1307&dpr=1"
+    result = requests.get(url).text 
+    doc = BeautifulSoup(result, "html.parser") 
+    span = doc.span
+    print(span)    
+webscrape_dinning_types()  
+
+
+#function to find the dining type from user input
 def find_restaurant():
     type = input("What kind of restaurant do you want to find? " )
     output = str("Here is a list of the " + type + " restaurants within a 10 mile radius of your area")
