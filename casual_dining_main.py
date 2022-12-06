e8417728a3da82354376d67767baf1fac4226133
Matthew Miller <mattmill1217@outlook.com>
2022-12-06 17:32:41 -0600

    added the beautiful soup module to webscrape from newegg.com

--- casual_dining_main.py
+++ casual_dining_main.py
@@ -1,37 +1,19 @@
-#from bs4 import BeautifulSoup
-#import requests
-#import re
+from bs4 import BeautifulSoup 
+import requests
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+from lxml import html
+import re
 
-#function to explain the program
-#def dining_types():
-    #print("This program will find all the type of restaurant's from your input")
+url = "https://www.newegg.com/p/pl?d=macbook"
+soup = BeautifulSoup(url, 'html.parser')
+content = soup.find('div', {"class": "item-cells-wrap border-cells items-grid-view four-cells expulsion-one-cell"})
 
-
-#web scraping function for dinning types.
-#-cjosh
-def webscrape_dinning_types():
-    from bs4 import BeautifulSoup 
-    import requests
-    from lxml import html
-    url = "https://www.newegg.com/p/pl?d=macbook"
-    tree = html.fromstring(page.content) 
-    result = requests.get(url).text 
-    macbook_type = tree.xpath('//a[@title="View Details"]/text()')
+page = ''
+for i in content.findAll('a'):
+    page = page + ' ' + i.text
+with open('scraped_text.txt', 'w') as file:
+    file.write(page)
 
 
-    print(macbook_type)
-
-
-
- 
-
-
-
-#function to find the dining type from user input
-#def find_restaurant():
- #   type = input("What kind of restaurant do you want to find? " )
-  #  output = str("Here is a list of the " + type + " restaurants within a 10 mile radius of your area")
-   # print(output)
-#find_restaurant()
-
-
+    
