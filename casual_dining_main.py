545e2fb554500b2670ace3282c081ed621d3d09e
Matthew Miller <mattmill1217@outlook.com>
2022-12-06 17:46:25 -0600

    I have no idea

--- casual_dining_main.py
+++ casual_dining_main.py
@@ -2,18 +2,20 @@ from bs4 import BeautifulSoup
 import requests
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
+import html5lib
 from lxml import html
 import re
+import csv 
+
 
 url = "https://www.newegg.com/p/pl?d=macbook"
 soup = BeautifulSoup(url, 'html.parser')
 content = soup.find('div', {"class": "item-cells-wrap border-cells items-grid-view four-cells expulsion-one-cell"})
 
 page = ''
-for i in content.findAll('a'):
-    page = page + ' ' + i.text
-with open('scraped_text.txt', 'w') as file:
-    file.write(page)
+for i in content.find('Macbook'):
+    #page = page + '' + i.text
+    with open('scraped_text.txt', 'w') as file:
+        file.write(page)
 
 
-    
