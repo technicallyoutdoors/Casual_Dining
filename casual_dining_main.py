5badde93131710d0ed242d9446db8ae101140182
Matthew Miller <mattmill1217@outlook.com>
2022-12-06 16:15:23 -0600

    added new egg webscraping

--- casual_dining_main.py
+++ casual_dining_main.py
@@ -3,9 +3,8 @@ import requests
 import re
 
 #function to explain the program
-def dining_types():
-    print("This program will find all the type of restaurant's from your input")
-dining_types()
+#def dining_types():
+    #print("This program will find all the type of restaurant's from your input")
 
 
 #web scraping function for dinning types.
@@ -13,21 +12,21 @@ dining_types()
 def webscrape_dinning_types():
     from bs4 import BeautifulSoup 
     import requests
-    url = "http://ajax.googleapis.com/ajax/services/search?rlz=1C1CHBF_enUS916US916&sxsrf=ALiCzsblr3vdeNM7Crb2YxKcokERB8vrIQ:1669440028825&q=North+Italia&ludocid=1882766253883301680&gsas=1&lsig=AB86z5UbFiwXMXCE3pI1C6IeWY-5&sa=X&ved=2ahUKEwj5_pDfjMv7AhXvm2oFHXTcCq8QhecIegQIahAN&biw=2560&bih=1307&dpr=1"
+    url = "https://www.newegg.com/p/pl?d=macbook"
     result = requests.get(url).text 
     doc = BeautifulSoup(result, "html.parser")
-    rest_type = doc.find(class_="zloOqf kpS1Ac vk_gy")
-    print(rest_type)
-    div = doc.div
-    print(span)    
+    macbook_type = doc.find(href="item-title")
+    print(macbook_type)
+    href = doc.href
+    print(href)    
 webscrape_dinning_types() 
 
 
 #function to find the dining type from user input
-def find_restaurant():
-    type = input("What kind of restaurant do you want to find? " )
-    output = str("Here is a list of the " + type + " restaurants within a 10 mile radius of your area")
-    print(output)
-find_restaurant()
+#def find_restaurant():
+ #   type = input("What kind of restaurant do you want to find? " )
+  #  output = str("Here is a list of the " + type + " restaurants within a 10 mile radius of your area")
+   # print(output)
+#find_restaurant()
 
 
